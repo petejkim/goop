6189b734ab3139aedc8f6a6cd3399007540479f2
Haleeq Usman <husman85@gmail.com>
2015-08-17 10:43:43 -0400

    It is useful to be able to specify the vendor path to use. This makes things easier when setting up custom go environments.

--- goop/goop_test.go
+++ goop/goop_test.go
@@ -1,11 +1,30 @@
-package goop_test
+package goop
 
 import (
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
+
+var _ = Describe("Goop", func() {
+	Describe("vendorDir()", func() {
+
+		It("Utilizes 'GOOP_VENDOR_DIR' environ variable when defined", func() {
+			os.Setenv("GOOP_VENDOR_DIR", "fake-goop-vendor-dir")
+			dir := NewGoop("./", os.Stdin, os.Stdout, os.Stderr).vendorDir()
+			Expect("fake-goop-vendor-dir").To(Equal(dir))
+		})
+
+		It("Defaults to '.vendors' in the base directory", func() {
+			os.Unsetenv("GOOP_VENDOR_DIR")
+			dir := NewGoop("/", os.Stdin, os.Stdout, os.Stderr).vendorDir()
+			Expect("/.vendor").To(Equal(dir))
+		})
+
+	})
+})
 
 func Test(t *testing.T) {
 	RegisterFailHandler(Fail)
